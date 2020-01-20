ea241209c263caba9d52190f59b5196ef1712543
Atri Das <babinsamrat123@gmail.com>
2020-01-20 08:40:37 +0530

    adds tutorial code

---
+++ app/src/main/java/com/example/kotlinsec2/MyNumbers.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinsec2
+
+data class MyNumbers(
+    var firstNum: String ="" ,
+    var secondNum:String ="",
+    var thirdNum:String ="",
+
+    var firstPlusOne:String ="",
+    var secondPlusOne:String ="",
+    var thirdPlusOne:String =""
+
+)
